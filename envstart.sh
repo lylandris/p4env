27e18bffcc7f5321ebae95515071c1c1c2f33687
Lylandris Jiang <lylandris.jiang@gmail.com>
2016-12-23 09:21:31 +0000

    add mnt of workspace

--- envstart.sh
+++ envstart.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+mkdir -p ${HOME}/workspace
+touch -a authorized_keys
+
 sudo chmod 600 authorized_keys && sudo chown root:root authorized_keys
-sudo docker run -d -p 22 --privileged=true -v ${PWD}/authorized_keys:/root/.ssh/authorized_keys lylandris/p4env:base
+sudo docker run -d -p 22 --privileged=true -v ${PWD}/authorized_keys:/root/.ssh/authorized_keys -v ${HOME}/workspace:/root/workspace lylandris/p4env:base
 
